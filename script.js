65ea03bdc71ddb27b0fe7dbff7fdea93fdce5877
Gustavo de Assis <gustavo.siqueira.421@gmail.com>
2022-08-24 22:31:11 -0300

    Layout com menus ao clicar

---
+++ script.js
@@ -0,0 +1,9 @@
+function abreMenu(){
+    const element = document.querySelector(".submenu");
+    element.classList.remove("escondido");
+}
+
+function fechaMenu(){
+    const element = document.querySelector(".submenu");
+    element.classList.add("escondido");
+}
